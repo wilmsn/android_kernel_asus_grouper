22016c9cd5e19ab956fc6f7c491c2ffcf0b66593
AndDiSa <anddisa@googlemail.com>
2017-08-11 16:08:17 +0200

    fix compile issues

--- arch/arm/mach-tegra/include/mach/dc.h
+++ arch/arm/mach-tegra/include/mach/dc.h
@@ -383,6 +383,7 @@ struct tegra_dc_out {
 	u8			*out_sel_configs;
 	unsigned		n_out_sel_configs;
 
+	int	(*prepoweroff)(void);
 	int	(*enable)(void);
 	int	(*postpoweron)(void);
 	int	(*disable)(void);
