a1b263138b43aca2ae3eea375c00f3502ebf622d
AndDiSa <anddisa@googlemail.com>
2017-08-25 11:31:42 +0200

    Revert "[PATCH] touchboost: squash the latest code from FK for Hammerhead"
    This reverts commit fb36ab4a7cdd59d31a467bb523980a45d5b674ed.

--- drivers/input/touchscreen/touchboost.c
+++ drivers/input/touchscreen/touchboost.c
@@ -14,45 +14,53 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/notifier.h>
+#include <linux/cpufreq.h>
 #include <linux/cpu.h>
 #include <linux/sched.h>
 #include <linux/jiffies.h>
+#include <linux/kthread.h>
+#include <linux/moduleparam.h>
+#include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/time.h>
-#include <linux/slab.h>
 
-#define MIN_TIME_INTERVAL_US (50 * USEC_PER_MSEC)
-
-struct touchboost_inputopen {
-	struct input_handle *handle;
-	struct work_struct inputopen_work;
-} touchboost_inputopen;
+#define MIM_TIME_INTERVAL_US (150 * USEC_PER_MSEC)
 
 /*
  * Use this variable in your governor of choice to calculate when the cpufreq
  * core is allowed to ramp the cpu down after an input event. That logic is done
  * by you, this var only outputs the last time in us an event was captured
  */
-static u64 last_input_time = 0;
+u64 last_input_time;
 
-inline u64 get_input_time(void)
-{
-	return last_input_time;
-}
+struct touchboost_inputopen {
+	struct input_handle *handle;
+	struct work_struct inputopen_work;
+} touchboost_inputopen;
 
 static void boost_input_event(struct input_handle *handle,
                 unsigned int type, unsigned int code, int value)
 {
 	u64 now;
 
-	if ((type == EV_ABS)) {
-		now = ktime_to_us(ktime_get());
+	now = ktime_to_us(ktime_get());
 
-		if (now - last_input_time < MIN_TIME_INTERVAL_US)
-			return;
+	if (now - last_input_time < MIM_TIME_INTERVAL_US)
+		return;
 
-		last_input_time = ktime_to_us(ktime_get());
-	}
+	last_input_time = ktime_to_us(ktime_get());
+}
+
+static void boost_input_open(struct work_struct *w)
+{
+	struct touchboost_inputopen *io =
+		container_of(w, struct touchboost_inputopen, inputopen_work);
+
+	int error;
+
+	error = input_open_device(io->handle);
+	if (error)
+		input_unregister_handle(io->handle);
 }
 
 static int boost_input_connect(struct input_handler *handler,
@@ -61,24 +69,20 @@ static int boost_input_connect(struct input_handler *handler,
 	struct input_handle *handle;
 	int error;
 
-	handle = kzalloc(sizeof(*handle), GFP_KERNEL);
-	if (handle == NULL)
+	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+	if (!handle)
 		return -ENOMEM;
 
 	handle->dev = dev;
 	handle->handler = handler;
-	handle->name = handler->name;
+	handle->name = "touchboost";
 
 	error = input_register_handle(handle);
 	if (error)
 		goto err;
 
-	error = input_open_device(handle);
-        if (error) {
-                input_unregister_handle(handle);
-		goto err;
-	}
-
+	touchboost_inputopen.handle = handle;
+	schedule_work(&touchboost_inputopen.inputopen_work);
 	return 0;
 
 err:
@@ -94,13 +98,27 @@ static void boost_input_disconnect(struct input_handle *handle)
 }
 
 static const struct input_device_id boost_ids[] = {
+	/* multi-touch touchscreen */
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT |
+			INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.evbit = { BIT_MASK(EV_ABS) },
+		.absbit = { [BIT_WORD(ABS_MT_POSITION_X)] =
+			BIT_MASK(ABS_MT_POSITION_X) |
+			BIT_MASK(ABS_MT_POSITION_Y) },
+	},
+	/* touchpad */
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_KEYBIT |
+			INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.keybit = { [BIT_WORD(BTN_TOUCH)] = BIT_MASK(BTN_TOUCH) },
+		.absbit = { [BIT_WORD(ABS_X)] =
+			BIT_MASK(ABS_X) | BIT_MASK(ABS_Y) },
+	},
+	/* Keypad */
 	{
 		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
-		.evbit = { BIT_MASK(EV_ABS) },
-		/* assumption: MT_.._X & MT_.._Y are in the same long */
-		.absbit = { [BIT_WORD(ABS_MT_POSITION_X)] =
-				BIT_MASK(ABS_MT_POSITION_X) |
-				BIT_MASK(ABS_MT_POSITION_Y) },
+		.evbit = { BIT_MASK(EV_KEY) },
 	},
 	{ },
 };
@@ -113,10 +131,11 @@ static struct input_handler boost_input_handler = {
 	.id_table       = boost_ids,
 };
 
-static int __init init(void)
+static int init(void)
 {
-	if (input_register_handler(&boost_input_handler))
-		pr_info("Unable to register the input handler\n");
+	INIT_WORK(&touchboost_inputopen.inputopen_work, boost_input_open);
+
+	input_register_handler(&boost_input_handler);
 
 	return 0;
 }
