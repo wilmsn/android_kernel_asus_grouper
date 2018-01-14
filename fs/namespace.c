0f5b92c8fafa453527c4c656a924938273426163
Al Viro <viro@zeniv.linux.org.uk>
2018-01-14 12:56:26 +0100

    vfs: spread struct mount - __propagate_umount() argument

--- fs/namespace.c
+++ fs/namespace.c
@@ -515,7 +515,7 @@ struct vfsmount *lookup_mnt(struct path *path)
 	br_read_lock(vfsmount_lock);
         child_mnt = __lookup_mnt(path->mnt, path->dentry, 1);
         if (child_mnt) {
-                mnt_add_count(child_mnt, 1);
+                mnt_add_count(&child_mnt->mnt, 1);
                 br_read_unlock(vfsmount_lock);
                 return &child_mnt->mnt;
         } else {
